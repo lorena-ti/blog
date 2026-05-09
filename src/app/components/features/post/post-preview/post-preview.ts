5e8dc82cb34adaffb80f669f6ddb86e9e43e8a07
Lorena Medeiros <lomedeiros@proton.me>
2026-05-09 16:45:55 -0300

    feat: add PostPreview component

---
+++ src/app/components/features/post/post-preview/post-preview.ts
@@ -0,0 +1,23 @@
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { PostData } from '../../../../models/post/post-data';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-post-preview',
+  imports: [DatePipe, MatCardModule, MatChipsModule],
+  templateUrl: './post-preview.html',
+  styleUrl: './post-preview.scss',
+})
+export class PostPreview {
+  @Input() post?: PostData;
+
+  constructor(private router: Router) { }
+
+  openPost() {
+    // When a post is selected, change the route to /artigos/:year/:month/:id/:slug
+    this.router.navigate(['artigos', this.post?.year, this.post?.month, this.post?.id, this.post?.slug]);
+  }
+}
