636b374409ae32465e3cf9e8ff9b789e7e05d2cf
Lorena Medeiros <lomedeiros@proton.me>
2026-05-10 20:47:03 -0300

    feat: define default page structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/ui/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('blog');
-}
+export class App { }
