64ecb73fb5a200719fbc0e12a75222f78ed306cf
Lorena Medeiros <lomedeiros@proton.me>
2026-05-10 15:55:46 -0300

    feat: add About component and define route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PostList } from '../app/components/features/post/post-list/post-list';
 import { Journal } from './pages/journal/journal';
 import { PostContent } from './components/features/post/post-content/post-content';
+import { About } from './components/features/explorer/about/about';
 
 export const routes: Routes = [
     { path: 'artigos', component: PostList },
@@ -11,4 +12,5 @@ export const routes: Routes = [
             { path: '', component: PostContent },
         ]
     },
+    { path: 'sobre', component: About },
 ];
