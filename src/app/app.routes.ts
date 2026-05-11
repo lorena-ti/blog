61c0de4a2d90da8a93df1fb320b27fcd767e9cfe
Lorena Medeiros <lomedeiros@proton.me>
2026-05-10 21:38:54 -0300

    feat: add Explorer page and define route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,23 @@ import { Journal } from './pages/journal/journal';
 import { PostContent } from './components/features/post/post-content/post-content';
 import { About } from './components/features/explorer/about/about';
 import { LabelList } from './components/features/explorer/label-list/label-list';
+import { Explorer } from './pages/explorer/explorer';
 
 export const routes: Routes = [
-    { path: 'artigos', component: PostList },
-    { path: '', redirectTo: 'artigos', pathMatch: 'full' },
+    {
+        path: '', component: Explorer, children: [
+            { path: '', redirectTo: 'artigos', pathMatch: 'full' },
+            { path: 'artigos', component: PostList },
+            { path: 'sobre', component: About },
+            { path: 'categorias', component: LabelList, data: { type: 'category' } },
+            { path: 'categorias/:category', component: PostList },
+            { path: 'tags', component: LabelList, data: { type: 'tag' } },
+            { path: 'tags/:tag', component: PostList },
+        ]
+    },
     {
         path: 'artigos/:year/:month/:id/:slug', component: Journal, children: [
             { path: '', component: PostContent },
         ]
     },
-    { path: 'sobre', component: About },
-    { path: 'categorias', component: LabelList, data: { type: 'category' } },
-    { path: 'categorias/:category', component: PostList },
-    { path: 'tags', component: LabelList, data: { type: 'tag' } },
-    { path: 'tags/:tag', component: PostList },
 ];
---
+++ src/app/pages/explorer/explorer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-explorer',
+  imports: [RouterOutlet],
+  templateUrl: './explorer.html',
+  styleUrl: './explorer.scss',
+})
+export class Explorer { }
