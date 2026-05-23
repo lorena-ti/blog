48503515e3503cda884953806a513d71f07e6642
Lorena Medeiros <lomedeiros@proton.me>
2026-05-23 17:24:48 -0300

    chore: register pt-BR locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import LocalePt from '@angular/common/locales/pt';
 
 import { routes } from './app.routes';
 import { provideMarkdown } from 'ngx-markdown';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(LocalePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideMarkdown()
+    provideMarkdown(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ]
 };
