a5fdec5a64010fa0fd597d7aeabae666fe8c7643
Lorena Medeiros <lomedeiros@proton.me>
2026-05-23 17:27:47 -0300

    chore: add mermaid and define dark theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import LocalePt from '@angular/common/locales/pt';
 
 import { routes } from './app.routes';
-import { provideMarkdown } from 'ngx-markdown';
+import { MERMAID_OPTIONS, provideMarkdown } from 'ngx-markdown';
 import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(LocalePt);
@@ -12,7 +12,14 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideMarkdown(),
+    provideMarkdown({
+      mermaidOptions: {
+        provide: MERMAID_OPTIONS,
+        useValue: {
+          theme: 'dark'
+        }
+      }
+    }),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
   ]
 };
