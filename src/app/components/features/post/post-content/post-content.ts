3c8ecc321ba07289cc24cab79cfe8f49ffca2647
Lorena Medeiros <lomedeiros@proton.me>
2026-05-23 17:35:07 -0300

    chore: add prismjs support to multiple languages and line numbers plugin

--- src/app/components/features/post/post-content/post-content.ts
+++ src/app/components/features/post/post-content/post-content.ts
@@ -12,6 +12,23 @@ import { Toc } from '../../../ui/toc/toc';
 import { BackArrow } from '../../../shared/back-arrow/back-arrow';
 import { Scroller } from '../../../../services/util/scroller';
 
+// PrismJS languages
+import 'prismjs/prism.js';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-latex';
+import 'prismjs/components/prism-yaml';
+import 'prismjs/components/prism-properties';
+
+// PrismJS plugins
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+
 @Component({
   selector: 'app-post-content',
   imports: [DatePipe, MatChipsModule, MatIconModule, MarkdownComponent, Toc, BackArrow],
